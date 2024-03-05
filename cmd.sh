7d2f846571ed15e9572e31936e5b2fb2496cad42
Max <max.hertenstein@gmx.de>
2024-03-05 20:32:28 +0100

    upate

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mysqld &
-mysql -u  root  -h  172.17.0.1 -p 3306 --protocol=tcp /app/create_user.sql
-java  -jar /app/ktor-mysql-backend.jar
+mysql -u  root  -h  172.17.0.1 -p 3306 --protocol=tcp /app/create_user.sql & 
+java  -jar /app/ktor-mysql-backend.jar & 
