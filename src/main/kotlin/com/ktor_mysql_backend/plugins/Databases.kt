c8025cf7fb2c0f2dca4d8a3c104954894b0121d1
Max <max.hertenstein@gmx.de>
2024-03-05 15:36:33 +0100

    update

--- src/main/kotlin/com/ktor_mysql_backend/plugins/Databases.kt
+++ src/main/kotlin/com/ktor_mysql_backend/plugins/Databases.kt
@@ -13,14 +13,14 @@ import java.io.File
 
 
 private fun prepareJDBI(slqScriptName: String): Jdbi {
-    val jdbi = Jdbi.create("jdbc:h2:mem:$slqScriptName;MODE=MYSQL;DB_CLOSE_DELAY=-1;", "username", "")
+    // val jdbi = Jdbi.create("jdbc:h2:mem:$slqScriptName;MODE=MYSQL;DB_CLOSE_DELAY=-1;", "username", "")
 //    val jdbi = Jdbi.create(
 //        "jdbc:mysql://localhost:3306/$slqScriptName?createDatabaseIfNotExist=true&autoReconnect=true", "max", "blabla"
 //    )
-//    Class.forName("com.mysql.cj.jdbc.Driver");
-//    val jdbi = Jdbi.create(
-//        "jdbc:mysql://172.17.0.1:3306/$slqScriptName?createDatabaseIfNotExist=true&autoReconnect=true", "max", "blabla"
-//    )
+   Class.forName("com.mysql.cj.jdbc.Driver");
+   val jdbi = Jdbi.create(
+       "jdbc:mysql://172.17.0.1:3306/$slqScriptName?createDatabaseIfNotExist=true&autoReconnect=true", "max", "blabla"
+   )
     jdbi.installPlugin(KotlinPlugin())
     jdbi.installPlugin(KotlinSqlObjectPlugin())
     val file = File("static/$slqScriptName.sql")
