03f80d7b81c7d95d4e2d0e11cf7efd3b909f3ea2
Max <max.hertenstein@gmx.de>
2024-03-05 16:11:10 +0100

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,4 @@
-import com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlExtension
 
-configure<AppEngineAppYamlExtension> {
-    stage {
-        setArtifact("build/libs/${project.name}-all.jar")
-    }
-    deploy {
-        version = "GCLOUD_CONFIG"
-        projectId = "GCLOUD_CONFIG"
-    }
-}
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -21,7 +11,6 @@ plugins {
     id("io.ktor.plugin") version "2.3.8"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("com.google.cloud.tools.appengine") version "2.4.2"
 }
 
 group = "com.ktor_mysql_backend"
@@ -29,7 +18,7 @@ version = "0.0.1"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-    //mainClass.set("com.example.ApplicationKt")
+//    mainClass.set("com.example.ApplicationKt")
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
@@ -47,7 +36,8 @@ dependencies {
 //    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("com.h2database:h2:$h2_version")
     implementation("io.ktor:ktor-server-call-logging-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
+//    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- src/main/kotlin/com/ktor_mysql_backend/Application.kt
+++ src/main/kotlin/com/ktor_mysql_backend/Application.kt
@@ -3,34 +3,40 @@ package com.ktor_mysql_backend
 import com.ktor_mysql_backend.plugins.*
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
+
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
-// fun Application.module() {
-//     install(CORS){//allowHeader(HttpHeaders.ContentType)
-//         allowHeader(HttpHeaders.AccessControlAllowOrigin)
-//         anyHost()}
-//     configureSerialization()
-//     configureDatabases()
-//     configureMonitoring()
-//     configureRouting()
-//     respondHTML ()
-// }
+ fun Application.module() {
+     install(CORS){//allowHeader(HttpHeaders.ContentType)
+         allowHeader(HttpHeaders.AccessControlAllowOrigin)
+         anyHost()}
+     configureSerialization()
+     configureDatabases()
+     configureMonitoring()
+     configureRouting()
+     respondHTML ()
+ }
 
-fun Application.module() {
-   embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+//fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-   install(CORS){//allowHeader(HttpHeaders.ContentType)
-       allowHeader(HttpHeaders.AccessControlAllowOrigin)
-       anyHost()}
-   configureSerialization()
-   configureDatabases()
-   configureMonitoring()
-   configureRouting()
-   respondHTML ()}
-}
+
+//fun main(args: Array<String>) {
+
+
+   
+
+//   embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+//       install(CORS){//allowHeader(HttpHeaders.ContentType)
+//           allowHeader(HttpHeaders.AccessControlAllowOrigin)
+//           anyHost()}
+//
+//   configureSerialization()
+//   configureDatabases()
+//   configureMonitoring()
+//   configureRouting()
+//   respondHTML ()}
+//}
