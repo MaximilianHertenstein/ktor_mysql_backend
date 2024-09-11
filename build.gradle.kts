ff10e361460c267eafe8ac66d57fe7db2efe546f
Max <max.hertenstein@gmx.de>
2024-09-11 08:22:58 +0200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ val h2_version: String by project
 val jdbi_version: String by project
 
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.20"
     id("io.ktor.plugin") version "2.3.12"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
