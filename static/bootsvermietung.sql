eff9ff8d6d67018bd71a0ed5dabe35b14e8049fc
Max <max.hertenstein@gmx.de>
2025-03-23 14:58:03 +0100

    add schule bootsvermietung

---
+++ static/bootsvermietung.sql
@@ -0,0 +1,140 @@
+-- ----------------------------------------------------------------------
+-- MySQL Migration Toolkit
+-- SQL Create Script
+-- ----------------------------------------------------------------------
+
+
+-- -------------------------------------
+-- Tables
+
+
+CREATE TABLE `bootstypen` (
+  `typnr` int NOT NULL,
+  `typbezeichnung` VARCHAR(50) NULL,
+   PRIMARY KEY (`typnr`)
+);
+
+
+
+CREATE TABLE `kunden` (
+  `kundennr` int NOT NULL,
+  `nachname` VARCHAR(30) NULL,
+  `vorname` VARCHAR(20) NULL,
+  PRIMARY KEY (`kundennr`)
+);
+
+CREATE TABLE `Boote` (
+  `bootsnr`int NOT NULL,
+  `bezeichnung` VARCHAR(50) NULL,
+  `stundenpreis` DOUBLE(4, 2) NULL,
+  `wert` DOUBLE(8, 2) NULL,
+  `typnr` int NULL,
+
+   PRIMARY KEY (`bootsnr`),
+   INDEX `BootstypenBoote` (`typnr`),
+  CONSTRAINT `BootstypenBoote` FOREIGN KEY `BootstypenBoote` (`typnr`)
+    REFERENCES `bootstypen` (`typnr`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
+  
+);
+
+
+CREATE TABLE `vermietungen` (
+  `mietnr` int NOT NULL,
+  `mietdatum` DATE NULL,
+  `mietdauer` INT NULL, 	
+  `bootsnr` int NULL,
+  `kundennr` int NULL,
+  PRIMARY KEY (`mietnr`),
+  INDEX `BooteVermietungen` (`bootsnr`),
+  INDEX `KundenVermietungen` (`kundennr`),
+  CONSTRAINT `BooteVermietungen` FOREIGN KEY `BooteVermietungen` (`bootsnr`)
+    REFERENCES `boote` (`bootsnr`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `KundenVermietungen` FOREIGN KEY `KundenVermietungen` (`kundennr`)
+    REFERENCES `kunden` (`kundennr`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
+)
+;
+
+
+
+-- ----------------------------------------------------------------------
+
+INSERT INTO `bootstypen`(`typnr`, `typbezeichnung`)
+VALUES
+  (1, 'Jolle'),
+  (2, 'Yacht'),
+  (3, 'Surfbrett'),
+  (4, 'Ruderboot');
+  
+
+
+INSERT INTO `kunden`(`kundennr`, `nachname`, `vorname`)
+VALUES
+  (1, 'Müller', 'Thomas'),
+  (2, 'Augenstein', 'Roland'),
+  (3, 'Noller', 'Andrea'),
+  (4, 'Girrbach', 'Christine'),
+  (5, 'Groß', 'Albert'),
+  (6, 'Hübener', 'Anton'),
+  (7, 'Schwarz', 'Toni'),
+  (8, 'Bernbacher', 'Günter'),
+  (9, 'Klittich', 'Rudolf'),
+  (10, 'Haider', 'Franz'),
+  (11, 'Brandauer', 'Gerd'),
+  (12, 'Thum', 'Joachim'),
+  (13, 'Reuter', 'Andreas'),
+  (14, 'Schneider', 'Torsten'),
+  (15, 'Winter', 'Ute'),
+  (16, 'Hermann', 'Gernott'),
+  (17, 'Martin', 'Uwe'),
+  (18, 'Boss', 'Karl'),
+  (19, 'Berner', 'Klara'),
+  (20, 'Büttner', 'Gerlinde'),
+  (21, 'Strecker', 'Tom'),
+  (22, 'Heck', 'Dieter'),
+  (23, 'Frühwind', 'Thomas'),
+  (24, 'Wengle', 'Hannes'),
+  (25, 'Albert', 'Rolf'),
+  (26, 'Fauser', 'Michael'),
+  (27, 'Fritz', 'Barbara');
+
+
+INSERT INTO `boote`(`bootsnr`, `bezeichnung`, `stundenpreis`, `wert`, `typnr`)
+VALUES
+  (1, 'Jollenkreuzer 15', 15, 12000,'1'),
+  (2, 'Sailhorse', 12, 8000,'1'),
+  (3, 'Sprinta DS', 20, 20000,'2'),
+  (4, 'Varianta 18', 18, 18500,'2'),
+  (5, 'Koralle', 8, 5000,'1'),
+  (6, 'Laser', 10, 7000,'1'),
+  (7, 'Sailart 18', 20, 30000,'2'),
+  (8, 'Sailart 20', 25, 40000,'2'),
+  (9, 'Ruderer1', 6, 4000,'4'),
+  (10, 'Ruderer2', 6, 4500,'4'),
+  (11, 'Pirat', 10, 7000,'1'),
+  (12, 'FD', 22, 7000,'1'),
+  (13, 'Highfly', 10, 2000,'3');
+
+
+  
+
+INSERT INTO `vermietungen`(`mietnr`, `mietdatum`, `mietdauer`, `bootsnr`, `kundennr`)
+VALUES 
+  (1, '2021-04-19 00:00:00', 4, 7, 9),
+  (2, '2021-04-24 00:00:00', 1, 4, 22),
+  (3, '2021-05-01 00:00:00', 2, 12, 15),
+  (4, '2021-05-05 00:00:00', 8, 3, 1),
+  (5, '2021-05-05 00:00:00', 7, 4, 20),
+  (6, '2021-05-06 00:00:00', 4, 7, 17),
+  (7, '2021-05-07 00:00:00', 6, 3, 20),
+  (8, '2021-05-14 00:00:00', 2, 7, 16),
+  (9, '2021-05-22 00:00:00', 1, 8, 10),
+  (10, '2021-05-30 00:00:00', 2, 11, 22),
+  (11, '2021-05-30 00:00:00', 2, 12, 1),
+  (12, '2021-05-02 00:00:00', 2, 2, 25),
+  (13, '2021-06-02 00:00:00', 2, 7, 18);
---
+++ static/schule.sql
@@ -0,0 +1,218 @@
+CREATE DATABASE  IF NOT EXISTS `schule` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `schule`;
+-- MySQL dump 10.16  Distrib 10.1.34-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: schule
+-- ------------------------------------------------------
+-- Server version	10.1.34-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `kursbelegungen`
+--
+
+DROP TABLE IF EXISTS `kursbelegungen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kursbelegungen` (
+  `belegungsNr` int(11) NOT NULL,
+  `schuelerNr` int(11) NOT NULL,
+  `kursNr` int(11) NOT NULL,
+  PRIMARY KEY (`belegungsNr`),
+  KEY `fk_kursbelegungen_schueler1_idx` (`schuelerNr`),
+  KEY `fk_kursbelegungen_kurse1_idx` (`kursNr`),
+  CONSTRAINT `fk_kursbelegungen_kurse1` FOREIGN KEY (`kursNr`) REFERENCES `kurse` (`kursNr`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_kursbelegungen_schueler1` FOREIGN KEY (`schuelerNr`) REFERENCES `schueler` (`schuelerNr`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `kurse`
+--
+
+DROP TABLE IF EXISTS `kurse`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kurse` (
+  `kursNr` int(11) NOT NULL,
+  `kursbezeichnung` varchar(45) DEFAULT NULL,
+  `lehrerNr` int(11) NOT NULL,
+  PRIMARY KEY (`kursNr`),
+  KEY `fk_kurse_lehrer1_idx` (`lehrerNr`),
+  CONSTRAINT `fk_kurse_lehrer1` FOREIGN KEY (`lehrerNr`) REFERENCES `lehrer` (`lehrerNr`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `lehrer`
+--
+
+DROP TABLE IF EXISTS `lehrer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lehrer` (
+  `lehrerNr` int(11) NOT NULL,
+  `nachname` varchar(45) DEFAULT NULL,
+  `vorname` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`lehrerNr`)
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `schueler`
+--
+
+DROP TABLE IF EXISTS `schueler`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schueler` (
+  `schuelerNr` int(11) NOT NULL,
+  `nachname` varchar(45) DEFAULT NULL,
+  `vorname` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`schuelerNr`)
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `unterrichte`
+--
+
+DROP TABLE IF EXISTS `unterrichte`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `unterrichte` (
+  `unterrichtsNr` int(11) NOT NULL,
+  `von` varchar(5) DEFAULT NULL,
+  `bis` varchar(5) DEFAULT NULL,
+  `wochentag` varchar(45) DEFAULT NULL,
+  `zimmerNr` int(11) NOT NULL,
+  `kursNr` int(11) NOT NULL,
+  PRIMARY KEY (`unterrichtsNr`),
+  KEY `fk_unterrichte_zimmer_idx` (`zimmerNr`),
+  KEY `fk_unterrichte_kurse1_idx` (`kursNr`),
+  CONSTRAINT `fk_unterrichte_kurse1` FOREIGN KEY (`kursNr`) REFERENCES `kurse` (`kursNr`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_unterrichte_zimmer` FOREIGN KEY (`zimmerNr`) REFERENCES `zimmer` (`zimmerNr`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `zimmer`
+--
+
+DROP TABLE IF EXISTS `zimmer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `zimmer` (
+  `zimmerNr` int(11) NOT NULL,
+  `raumbezeichnung` varchar(45) DEFAULT NULL,
+  `anzahl_plaetze` int(11) DEFAULT NULL,
+  PRIMARY KEY (`zimmerNr`)
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-25 11:45:51
+
+
+-- MySQL dump 10.16  Distrib 10.1.34-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: schule
+-- ------------------------------------------------------
+-- Server version	10.1.34-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `kursbelegungen`
+--
+
+
+/*!40000 ALTER TABLE `kursbelegungen` DISABLE KEYS */;
+INSERT INTO `kursbelegungen` VALUES (1,1,3),(2,1,6),(3,2,2),(4,2,4),(5,3,4),(6,3,6),(7,4,1),(8,4,3),(9,5,2),(10,5,5),(11,6,4),(12,6,6),(13,7,3),(14,7,1),(15,8,4),(16,8,5),(17,9,3),(18,9,6),(19,10,4),(20,10,5),(21,11,3),(22,11,6),(23,12,4),(24,12,5),(25,13,2),(26,13,4),(27,14,1),(28,14,4),(29,15,2),(30,15,5),(31,16,1),(32,16,6),(33,17,3),(34,17,5),(35,18,3),(36,18,6),(37,19,4),(38,19,5),(39,20,4),(40,20,6),(41,21,2),(42,21,3),(43,22,2),(44,22,4),(45,23,1),(46,23,3),(47,24,1),(48,24,4),(49,25,3),(50,25,5),(51,26,4),(52,26,6),(53,27,3),(54,27,5),(55,28,4),(56,28,6),(57,29,1),(58,29,5),(59,30,1),(60,30,6),(61,31,2),(62,31,5),(63,32,2),(64,32,6),(65,33,1),(66,33,3),(67,34,2),(68,34,4),(69,35,3),(70,35,5),(71,36,4),(72,36,6),(73,37,1),(74,37,5),(75,38,2),(76,38,6),(77,39,3),(78,39,6),(79,40,4),(80,40,5),(81,41,2),(82,41,3),(83,42,1),(84,42,4),(85,43,3),(86,43,5),(87,44,3),(88,44,6),(89,45,4),(90,45,6),(91,46,2),(92,46,4);
+/*!40000 ALTER TABLE `kursbelegungen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `kurse`
+--
+
+
+/*!40000 ALTER TABLE `kurse` DISABLE KEYS */;
+INSERT INTO `kurse` VALUES (1,'Informatik1',2),(2,'Informatik2',3),(3,'Mathematik1',1),(4,'Mathematik2',2),(5,'Deutsch1',3),(6,'Deutsch2',1);
+/*!40000 ALTER TABLE `kurse` ENABLE KEYS */;
+
+
+--
+-- Dumping data for table `lehrer`
+--
+
+
+/*!40000 ALTER TABLE `lehrer` DISABLE KEYS */;
+INSERT INTO `lehrer` VALUES (1,'Maier','Jochen'),(2,'Brandt','Sabine'),(3,'Jundt','Michael');
+/*!40000 ALTER TABLE `lehrer` ENABLE KEYS */;
+
+
+--
+-- Dumping data for table `schueler`
+--
+
+
+/*!40000 ALTER TABLE `schueler` DISABLE KEYS */;
+INSERT INTO `schueler` VALUES (1,'Pfeifer','Leon'),(2,'Horn','Julia'),(3,'Klatt','Maren'),(4,'Schneider','Sophie'),(5,'Strick','Jana'),(6,'Florath','Fabian'),(7,'Heine','Markus'),(8,'Frey','Nadine'),(9,'Maler','Michael'),(10,'Schmidt','Fabienne'),(11,'Schwab','Sina'),(12,'Richter','Mirko'),(13,'Bauer','Stefan'),(14,'Bernhard','Marina'),(15,'Voss','Stefanie'),(16,'Fischer','Theo'),(17,'Weber','Kathrin'),(18,'Wirth','Mike'),(19,'Becker','Sandra'),(20,'Stark','Bernd'),(21,'Hoffmann','Jan'),(22,'Krause','Lara'),(23,'König','Janina'),(24,'Zeller','Ralf'),(25,'Ackermann','Maike'),(26,'Braun','Anna'),(27,'Lippert','Sina'),(28,'Peters','Angelika'),(29,'Fuchs','Daniel'),(30,'Wulf','Stefanie'),(31,'Bartsch','Dirk'),(32,'Köhler','Moritz'),(33,'Hinz','Julia'),(34,'Döring','Melanie'),(35,'Ehrhardt','Frank'),(36,'Böhm','Luisa'),(37,'Winter','Alexandra'),(38,'Uhl','Ulrike'),(39,'Hempel','Paul'),(40,'Reich','Matthias'),(41,'Schumacher','Eva'),(42,'Schuster','Joel'),(43,'Vogt','Anina'),(44,'Kieler','Rahel'),(45,'Witte','Sarah'),(46,'Horn','Tanja');
+/*!40000 ALTER TABLE `schueler` ENABLE KEYS */;
+
+
+--
+-- Dumping data for table `unterrichte`
+--
+
+
+/*!40000 ALTER TABLE `unterrichte` DISABLE KEYS */;
+INSERT INTO `unterrichte` VALUES (1,'13:45','15:15','Mittwoch',1,1),(2,'9:35','11:05','Freitag',1,2),(3,'7:45','9:15','Montag',2,3),(4,'9:35','11:05','Donnerstag',2,3),(5,'7:45','9:15','Dienstag',2,4),(6,'7:45','9:15','Freitag',3,4),(7,'9:35','11:05','Montag',3,5),(8,'11:20','12:50','Freitag',3,5),(9,'11:20','12:50','Dienstag',2,6),(10,'7:45','9:15','Donnerstag',3,6);
+/*!40000 ALTER TABLE `unterrichte` ENABLE KEYS */;
+
+
+--
+-- Dumping data for table `zimmer`
+--
+
+/*!40000 ALTER TABLE `zimmer` DISABLE KEYS */;
+INSERT INTO `zimmer` VALUES (1,'101',20),(2,'117',30),(3,'118',29);
+/*!40000 ALTER TABLE `zimmer` ENABLE KEYS */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-25 11:46:50
